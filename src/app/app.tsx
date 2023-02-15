f3d55c57c793e74e557c9dc503a76e8d465da6ac
develtype <develtype@gmail.com>
2023-02-15 14:31:08 +0900

    feat: init router and temporary nav component

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,25 @@
 import 'src/style/css/common.scss';
 import React from 'react';
+import { NavBar } from 'src/component/organism/nav-bar/nav-bar';
+import { NavItemType } from 'src/type/common.type';
+import { AppRoute } from './app.route';
 import styles from './app.scss';
 
 export const App = () => (
   <div className={styles.root}>
-    <h1>Webpack react template!</h1>
+    <NavBar navItems={NavItems} />
+    <header className={styles.title}>Webpack react template!</header>
+    <AppRoute />
   </div>
 );
+
+const NavItems: NavItemType[] = [
+  {
+    to: '/',
+    content: 'Home',
+  },
+  {
+    to: '/button',
+    content: 'Buttons',
+  },
+];
---
+++ src/page/button/button.page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const ButtonPage = () => {
+  return (
+    <div>
+      <div>Button Components</div>
+    </div>
+  );
+};
